fa68e9cf06c3276bec2751aa7bb07fe7f029fa01
David M Gudeman <davidmgudeman@gmail.com>
2015-05-09 18:02:56 -0700

    everything works but can't delete one item

---
+++ src/FHlazySearchTree.java
@@ -0,0 +1,298 @@
+
+import cs_1c.*;
+
+import java.util.*;
+
+
+
+public class FHlazySearchTree<E extends Comparable< ? super E > >
+   implements Cloneable
+{
+   protected int mSize;
+   protected int mSizeHard;
+   protected FHlazySTNode<E> mRoot;
+   
+   public FHlazySearchTree() { clear(); }
+   public boolean empty() { return (mSize == 0); }
+   public int size() { return mSize; }
+   public void clear() { mSize = 0; mRoot = null; }
+   public int showHeight() { return findHeight(mRoot, -1); }
+   
+   public E findMin() 
+   {
+      if (mRoot == null)
+         throw new NoSuchElementException();
+      return findMin(mRoot).data;
+   }
+   
+   public E findMax() 
+   {
+      if (mRoot == null)
+         throw new NoSuchElementException();
+      return findMax(mRoot).data;
+   }
+   
+   public E find( E x )
+   {
+      FHlazySTNode<E> resultNode;
+      resultNode = find(mRoot, x);
+      if (resultNode == null)
+         throw new NoSuchElementException();
+      return resultNode.data;
+   }
+   public boolean contains(E x)  { return find(mRoot, x) != null; }
+   
+   public boolean insert( E x )
+   {
+      int oldSize = mSize;
+      mRoot = insert(mRoot, x);
+      mSize++;
+      mSizeHard++;
+      return (mSize != oldSize);
+      
+   }
+  
+   public boolean remove( E x )
+   {
+      int oldSize = mSize;
+      remove(mRoot, x);
+      return (mSize != oldSize);
+   }
+   
+   public boolean collectGarbage()
+   {
+      int oldSizeHard = mSizeHard;
+      collectGarbage(mRoot); 
+      return (mSizeHard != oldSizeHard);
+   }
+   
+   public < F extends Traverser<? super E > > 
+   void traverse(F func)
+   {
+      traverse(func, mRoot);
+   }
+   
+   public Object clone() throws CloneNotSupportedException
+   {
+      FHlazySearchTree<E> newObject = (FHlazySearchTree<E>)super.clone();
+      newObject.clear();  // can't point to other's data
+
+      newObject.mRoot = cloneSubtree(mRoot);
+      newObject.mSize = mSize;
+      newObject.mSizeHard = mSizeHard;
+      
+      return newObject;
+   }
+   
+   // private helper methods ----------------------------------------
+   protected FHlazySTNode<E> findMin( FHlazySTNode<E> root ) 
+   {
+      if (root == null)
+         return null;
+      if (root.lftChild == null)
+         return root;
+      return findMin(root.lftChild);
+   }
+   
+   protected FHlazySTNode<E> findMax( FHlazySTNode<E> root ) 
+   {
+      if (root == null)
+         return null;
+      if (root.rtChild == null)
+         return root;
+      return findMax(root.rtChild);
+   }
+   
+   protected FHlazySTNode<E> insert( FHlazySTNode<E> root, E x )
+   {
+      int compareResult;  // avoid multiple calls to compareTo()
+      
+      if (root == null)
+      {
+         return new FHlazySTNode<E>(x, null, null, false);
+      }
+      
+      compareResult = x.compareTo(root.data); 
+      if ( compareResult < 0 )
+         root.lftChild = insert(root.lftChild, x);
+      else if ( compareResult > 0 )
+         root.rtChild = insert(root.rtChild, x);
+      else if ( compareResult == 0)
+      { 
+         if (root.deleted == true)
+         {root.deleted = false;
+         mSizeHard--;
+         return root;
+         }
+         mSize--;
+         mSizeHard--;
+         return root;
+      }
+      
+      return root;
+   }
+     
+   protected void remove( FHlazySTNode<E> root, E x  )
+   {
+      int compareResult;  // avoid multiple calls to compareTo()
+      
+      if (root == null)
+         return;
+      
+      compareResult = x.compareTo(root.data); 
+      if ( compareResult < 0 )
+         remove(root.lftChild, x);
+      else if ( compareResult > 0 )
+         remove(root.rtChild, x);  
+      else if (root.deleted != true)
+      {
+         root.deleted = true;      
+         mSize--;
+      }
+   }
+   
+   protected FHlazySTNode<E> collectGarbage(FHlazySTNode<E> root)
+   {
+      if (root == null)
+       return null;
+      
+      root = removeHard(root);
+      root.lftChild = collectGarbage(removeHard(root.lftChild));
+      root.rtChild = collectGarbage(removeHard(root.rtChild));
+         return root;
+   }
+
+   protected FHlazySTNode<E> removeHard ( FHlazySTNode<E> root)
+   {
+     
+      if (root == null)
+         return null;
+      if (root.deleted == false)
+         return root;
+
+      else if (root.lftChild != null && root.rtChild != null)
+      {   
+         FHlazySTNode<E> tempNode = findMin(root.rtChild);
+         if (tempNode.deleted ==true)
+         { 
+            tempNode = null;
+            return removeHard(root);
+         }
+         root.data = root.rtChild.data;
+         root.deleted = false;
+         root.rtChild.deleted = true;       
+      }
+      else if (root.lftChild == null && root.rtChild != null)
+      {
+         root.data = root.rtChild.data;
+         root.deleted = root.rtChild.deleted;
+         root.rtChild.deleted = true;            
+      }
+      else if (root.rtChild == null && root.lftChild != null)
+      {
+         root.data = root.lftChild.data;
+         root.deleted = root.lftChild.deleted;;
+         root.lftChild.deleted = true;        
+      }
+      else if(root.rtChild == null && root.lftChild == null)
+      {
+         mSizeHard--;
+         root = null;
+         return null;   
+      }
+             
+
+    return root;
+      
+      
+   }
+   
+   protected <F extends Traverser<? super E>> 
+   void traverse(F func, FHlazySTNode<E> treeNode)
+   {
+      if (treeNode == null)
+         return;
+
+      traverse(func, treeNode.lftChild);
+      if (treeNode.deleted != true) {func.visit(treeNode.data);};
+      traverse(func, treeNode.rtChild);
+   }
+   
+   protected FHlazySTNode<E> find( FHlazySTNode<E> root, E x )
+   {
+      int compareResult;  // avoid multiple calls to compareTo()
+
+      if (root == null)
+         return null;
+
+      compareResult = x.compareTo(root.data); 
+      if (compareResult < 0)
+         return find(root.lftChild, x);
+      if (compareResult > 0)
+         return find(root.rtChild, x);
+      return root;   // found
+   }
+   
+   protected FHlazySTNode<E> cloneSubtree(FHlazySTNode<E> root)
+   {
+      FHlazySTNode<E> newNode;
+      if (root == null)
+         return null;
+
+      // does not set myRoot which must be done by caller
+      newNode = new FHlazySTNode<E>
+      (
+         root.data, 
+         cloneSubtree(root.lftChild), 
+         cloneSubtree(root.rtChild),
+         root.deleted
+      );
+      return newNode;
+   }
+   
+   protected int findHeight( FHlazySTNode<E> treeNode, int height ) 
+   {
+      int leftHeight, rightHeight;
+      if (treeNode == null)
+         return height;
+      height++;
+      leftHeight = findHeight(treeNode.lftChild, height);
+      rightHeight = findHeight(treeNode.rtChild, height);
+      return (leftHeight > rightHeight)? leftHeight : rightHeight;
+   }
+   
+
+   protected int sizeHard()
+   {
+      return mSizeHard;
+   }
+}
+
+
+//4B
+class FHlazySTNode<E extends Comparable< ? super E > >
+{
+   // use public access so the tree or other classes can access members 
+   public FHlazySTNode<E> lftChild, rtChild;
+   public E data;
+   public FHlazySTNode<E> myRoot;  // needed to test for certain error
+   public boolean deleted;
+
+   public FHlazySTNode( E d, FHlazySTNode<E> lft, FHlazySTNode<E> rt, boolean del)
+   {
+      lftChild = lft; 
+      rtChild = rt;
+      data = d;
+      deleted = del;
+   }
+   
+   public FHlazySTNode()
+   {
+      this(null, null, null, false);
+   }
+   
+   // function stubs -- for use only with AVL Trees when we extend
+   public int getHeight() { return 0; }
+   boolean setHeight(int height) { return true; }
+}
+
---
+++ src/Foothill.java
@@ -0,0 +1,332 @@
+
+
+
+import cs_1c.*;
+
+class PrintObject<E> implements Traverser<E>
+{
+public void visit(E x)
+{
+   System.out.print( x + " ");
+}
+};
+
+//------------------------------------------------------
+public class Foothill
+{
+// -------  main --------------
+public static void main(String[] args) throws Exception
+{
+   int k;
+   FHlazySearchTree<Integer> searchTree = new FHlazySearchTree<Integer>();
+   PrintObject<Integer> intPrinter = new PrintObject<Integer>();
+
+   searchTree.traverse(intPrinter);
+
+   System.out.println( "\ninitial size: " + searchTree.size() );
+   searchTree.insert(50);
+   searchTree.insert(20);
+   searchTree.insert(61);
+   searchTree.insert(30);
+   searchTree.insert(59);
+   searchTree.insert(10);
+   searchTree.insert(9);
+   searchTree.insert(60);
+   searchTree.insert(70);
+   searchTree.insert(62);
+
+  
+   //TRAVERSE
+   System.out.println( "\nTRAVERSE after populating: ");
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   //SOFT REMOVAL
+   System.out.println( "\n59 (one child) soft removal: ");
+   if (searchTree.remove(59))
+      System.out.println( "removed " + 59);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   //SOFT REMOVAL
+   System.out.println( "\n20 (two children) soft removal: ");
+   if (searchTree.remove(20))
+      System.out.println( "removed " + 20 );
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   //SOFT REMOVAL
+   System.out.println( "\n62 (leaf) soft removal: ");
+   if (searchTree.remove(62))
+      System.out.println( "removed " + 62 );
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+  
+   //GARBAGE COLLECTION
+   System.out.println( "\nCollecting garbage after soft remove of 3 items.\n"
+         + "Size should be 7 and Hard Size should be 7. " );
+   searchTree.collectGarbage();
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   //GARBAGE COLLECTION
+   System.out.println( "\nCollecting garbage REPEAT. Should be no change" );
+   searchTree.collectGarbage();
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println("\n\nREINSERTING AFTER A SOFT REMOVAL");
+   
+   //SOFT REMOVAL
+   System.out.println( "\nAttempting 9 soft removal: ");
+   searchTree.remove(9);
+   System.out.println( "removed " + 9 );
+   System.out.println( "tree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+ 
+   System.out.println( "\nReinsert 9 which is in a soft delete state.\n"
+      + "Should only increment the size not the hard size.");  
+   searchTree.insert(9);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println( "\ninsert 9 again.\n");  
+      searchTree.insert(9);
+      System.out.println( "\ntree 1 size: " + searchTree.size() 
+         + "  Hard size: " + searchTree.sizeHard() );
+   
+    // test assignment operator
+   System.out.println("\nClone Search Tree (ST) into SEarch Tree 2 (ST2)\n");
+   FHlazySearchTree<Integer> searchTree2 
+      = (FHlazySearchTree<Integer>)searchTree.clone();
+    
+   //TRAVERSE
+   System.out.println( "\nSearch Tree (ST) now: ");
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+ 
+   //TRAVERSE
+   System.out.println( "\nSearch Tree 2 (ST2), a product of clone: ");
+   searchTree2.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree2.size() 
+      + "  Hard size: " + searchTree2.sizeHard() );
+    
+   System.out.println("Six insertions into ST2\n");
+   searchTree2.insert(500);
+   searchTree2.insert(200);
+   searchTree2.insert(300);
+   searchTree2.insert(700);
+   searchTree2.insert(100);
+   searchTree2.insert(600);
+    
+ //TRAVERSE
+   System.out.println( "\nST2 now: ");
+   searchTree2.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree2.size() 
+       + "  Hard size: " + searchTree2.sizeHard() );
+   
+   //TRAVERSE
+   System.out.println( "\nST now: ");
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+  
+   //SOFT REMOVAL 60 FROM ST
+   System.out.println( "\n60 soft removal from ST: ");
+   if (searchTree.remove(60))
+   System.out.println( "removed " + 60);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\nST size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   //SOFT REMOVAL 100 FROM ST2
+   System.out.println( "\n100 soft removal from ST2: ");
+   if (searchTree2.remove(100))
+   System.out.println( "soft removed " + 100);
+   
+   System.out.println( "\nST2 now: ");
+   searchTree2.traverse(intPrinter);
+   System.out.println( "\nST2 size: " + searchTree2.size() 
+      + "  Hard size: " + searchTree2.sizeHard() );
+    
+   // test soft insertion and deletion:
+   
+   // INSERT
+   System.out.println( "\nST size: " + searchTree.size() 
+         + "  Hard size: " + searchTree.sizeHard() );
+   System.out.println( "\nAdd 22 to ST " );
+   searchTree.insert(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\nST size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   
+   System.out.println( "\nST after soft removal 22. " );
+   searchTree.remove(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nST repeating soft removal 22. Should see no "
+         + "change. " );
+   searchTree.remove(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nSoft insertion. Hard size should not change. " );
+   searchTree.insert(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nAttempting 1 removal: ");
+   if (searchTree.remove(22))
+      System.out.println( "removed " + 22 );
+   searchTree.traverse(intPrinter);
+   System.out.println( "tree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nCollecting Garbage - should clean 2 items. " );
+   searchTree.collectGarbage();
+   System.out.println( "tree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nCollecting Garbage again - no change expected. " );
+   searchTree.collectGarbage();
+   System.out.println( "tree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   // test soft insertion and deletion:
+
+   System.out.println( "\nAdding 'hard' 22 - should see new sizes. " );
+   searchTree.insert(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nAfter soft removal. " );
+   searchTree.remove(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nRepeating soft removal. Should see no change. " );
+   searchTree.remove(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\nSoft insertion. Hard size should not change. " );
+   searchTree.insert(22);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   System.out.println( "\n\nAttempting 100 removals: " );
+   for (k = 100; k > 50; k--)
+   {
+      if (searchTree.remove(k))
+         System.out.println( "removed " + k );
+   }
+   
+   System.out.println("\nGARBAGE COLLECTION");
+   searchTree.collectGarbage();
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+         + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println("\nGARBAGE COLLECTION");
+   searchTree.collectGarbage();
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+         + "  Hard size: " + searchTree.sizeHard() );
+   
+  
+   System.out.println( "\nAfter soft removal 9 " );
+   searchTree.remove(9);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println( "\nAfter soft removal 10 " );
+   searchTree.remove(10);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+//   System.out.println( "\nAfter soft removal 22 " );
+//   searchTree.remove(22);
+//   searchTree.traverse(intPrinter);
+//   System.out.println( "\ntree 1 size: " + searchTree.size() 
+//      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println( "\nAfter soft removal 30" );
+   searchTree.remove(30);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println( "\nAfter soft removal 50 " );
+   searchTree.remove(50);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println( "\nAfter soft removal 61 " );
+   searchTree.remove(61);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+   
+   System.out.println( "\nAfter soft removal 70 " );
+   searchTree.remove(70);
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+      + "  Hard size: " + searchTree.sizeHard() );
+
+   
+   System.out.println("\nPRINT");
+   searchTree.traverse(intPrinter);
+   System.out.println("\nGARBAGE COLLECTION");
+   searchTree.collectGarbage();
+   searchTree.traverse(intPrinter);
+   System.out.println( "\ntree 1 size: " + searchTree.size() 
+         + "  Hard size: " + searchTree.sizeHard() );
+   
+//   System.out.println("\nGARBAGE COLLECTION");
+//   searchTree.collectGarbage();
+//   searchTree.traverse(intPrinter);
+//   System.out.println("\nGARBAGE COLLECTION");
+//   searchTree.collectGarbage();
+//   searchTree.traverse(intPrinter);
+//   System.out.println("\nGARBAGE COLLECTION");
+//   System.out.println( "\ntree size: " + searchTree.size() 
+//      + "  Hard size: " + searchTree.sizeHard() );
+//
+//
+//   System.out.println("\nFIND MAX" + searchTree.findMax());
+//   System.out.println("\nFIND MIN" + searchTree.findMin());
+//   
+//   System.out.println( "\nRemove 50. " );
+//   searchTree.remove(50);
+//   searchTree.traverse(intPrinter);
+//   System.out.println( "\ntree 1 size: " + searchTree.size() 
+//      + "  Hard size: " + searchTree.sizeHard() );
+//
+//   System.out.println( "\nCollecting Garbage after remove 50. " );
+//   searchTree.collectGarbage();
+//   searchTree.traverse(intPrinter);
+//   System.out.println( "tree 1 size: " + searchTree.size() 
+//      + "  Hard size: " + searchTree.sizeHard() );  
+//   System.out.println("ASSIGNMENT 4B");
+}
+
+}
